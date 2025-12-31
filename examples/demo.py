43be1bcd2334f006b8486b44c1694118aa0250a3
TechRuler <ask995844@gmail.com>
2026-01-01 02:27:38 +0530

    some changes

--- examples/demo.py
+++ examples/demo.py
@@ -3,10 +3,9 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from tkeditor import Editor
-from tkeditor.lexers import PythonLexer,Highlighter,HTMLLexer
-from tkeditor.lexers.pythonlexer import PythonLexer
-from tkinter import Tk, Frame
-# Frame()
+from tkeditor.lexers import PythonLexer,Highlighter
+from tkinter import Tk
+
 root = Tk()
 root.geometry("800x500") 
 root.title("tkeditor")
@@ -21,5 +20,9 @@ editor.pack(expand=True, fill="both")
 
 
 lexer = PythonLexer()
+lexer.set_styles(
+    keyword={"foreground":"tomato"},
+    builtin={"foreground":"steelblue"}
+)
 highlight = Highlighter(editor.get_text_widget(), lexer)
 root.mainloop()
