108c64415e2ea750db0ece6c7a22a1158326ac89
TechRuler <ask995844@gmail.com>
2026-01-01 02:28:44 +0530

    some changes

--- examples/demo.py
+++ examples/demo.py
@@ -2,27 +2,39 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
+import tkinter as tk
 from tkeditor import Editor
 from tkeditor.lexers import PythonLexer,Highlighter
-from tkinter import Tk
 
-root = Tk()
+root = tk.Tk()
 root.geometry("800x500") 
 root.title("tkeditor")
 editor = Editor(
-    root, 
-    font=("Cascadia Code",15), 
-    indent_line_color="red", 
-    bracket_tracker=True
+    root,
+    linenumber=True,
+    folder_code=True,
+    wrap="none",
+    font=("Consolas", 12),
 )
 
 editor.pack(expand=True, fill="both")
 
 
 lexer = PythonLexer()
-lexer.set_styles(
-    keyword={"foreground":"tomato"},
-    builtin={"foreground":"steelblue"}
-)
+
+# set custom syntax colors
+# method 1
+# lexer.set_styles(
+#     keyword={"foreground":"tomato"},
+#     builtin={"foreground":"steelblue"}
+# )
+# method 2 
+# lexer.set_styles({
+#     "keyword":{"foreground":"tomato"},
+#     "builtin":{"foreground":"steelblue"}
+#     }
+# )
+# Note you can also use both methods at same time
+
 highlight = Highlighter(editor.get_text_widget(), lexer)
 root.mainloop()
